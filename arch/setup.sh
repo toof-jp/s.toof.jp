4d63d16dd08a94c4702de30ba3bc8d066d11bbad
toof <toof@toof.jp>
2025-06-08 06:27:26 +0900

    Update setup.sh

--- arch/setup.sh
+++ arch/setup.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+set -x
+
+# install paru
 sudo pacman -S --needed git base-devel rustup
 rustup default nightly
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
+
+# generate ssh key
+
+set +x
