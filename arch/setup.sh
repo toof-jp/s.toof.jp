11e030b7cbbf1afd09727be9a329e926e381edf4
toof <toof@toof.jp>
2025-06-08 06:32:25 +0900

    Update setup.sh

--- arch/setup.sh
+++ arch/setup.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 set -x
 
-# install paru
+# paru
 sudo pacman -S --needed git base-devel rustup
 rustup default nightly
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
+cd
 
-# generate ssh key
-
+# ssh key
+ssh-keygen -t ed25519 -C "toof@toof.jp"
+sudo pacman -S github-cli
+gh auth login
+gh ssh-key add ~/.ssh/id_ed25519.pub --title "$(hostname)"
 set +x
