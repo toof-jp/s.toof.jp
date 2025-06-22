d7d25c5dac2896e8b4b9097dd9fdf55132a40d8f
toof <toof@toof.jp>
2025-06-22 22:12:38 +0900

    update

---
+++ macos/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -x
+
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# generate ssh key
+ssh-keygen -f ~/.ssh/id_ed25519 -t ed25519 -C "toof@toof.jp"
+
+# upload public key
+gh auth login --git-protocol ssh
+
+ghq clone git@github.com:toof-jp/dotfiles.git
+cd ~/ghq/github.com/toof-jp/dotfiles
+make
+
+ghq clone git@github.com:toof-jp/github-cli-extension-list.git
+cd ~/ghq/github.com/toof-jp/github-cli-extension-list
+sh install.sh
+
+set +x
