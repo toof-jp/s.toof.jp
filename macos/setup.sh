aa0c530b1c15274cc944fe5dc8738a2266470585
toof <toof@toof.jp>
2025-06-23 21:51:13 +0900

    update

--- macos/setup.sh
+++ macos/setup.sh
@@ -9,6 +9,11 @@ ssh-keygen -f ~/.ssh/id_ed25519 -t ed25519 -C "toof@toof.jp"
 # upload public key
 gh auth login --git-protocol ssh
 
+ghq clone git@github.com:toof-jp/macos-package-list.git
+cd ~/ghq/github.com/toof-jp/macos-package-list
+sh brew.sh
+sh mas.sh
+
 ghq clone git@github.com:toof-jp/dotfiles.git
 cd ~/ghq/github.com/toof-jp/dotfiles
 make
